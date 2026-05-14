8fedf52581172e28a83d6d1424e00badd706bbd3
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-05-15 01:53:21 +0900

    Feat: User Service Feign Client 인터페이스
    - CredentialVerificationResponse ->  UserAuthInfoResponse 이름 변경
    - UserServiceClient decode config 추가
    - 권한 조회 추가 "internal/users/{userId}/auth-info"
    
    Related to:#6

--- src/main/java/com/pagely/authservice/infrastructure/client/dto/UserAuthInfoResponse.java
+++ src/main/java/com/pagely/authservice/infrastructure/client/dto/UserAuthInfoResponse.java
@@ -4,9 +4,9 @@ import com.pagely.common.auth.Role;
 import java.util.UUID;
 
 /**
- * User Service 의 자격 검증 API 응답 DTO.
+ * User Service 의 자격 검증, 권한 조회 API 응답 DTO.
  */
-public record CredentialVerificationResponse(
+public record UserAuthInfoResponse(
         UUID userId,
         Role role
 ) {
